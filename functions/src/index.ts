12bdafba1e36360e26f3263b150bbf6a1735df7b
robertIsaac <robert.isaac@live.com>
2021-05-19 23:21:46 +0200

    add algoliasearch

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,24 @@
 import * as functions from "firebase-functions";
+import algoliasearch from "algoliasearch";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const ALGOLIA_ID = functions.config().algolia.app_id;
+const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
+
+const ALGOLIA_INDEX_NAME = "algolia-firestore";
+const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
+
+// Update the search index every time a blog post is written.
+exports.onQuestionCreated =
+  functions.firestore.document("questions/{questionId}").onCreate(
+      (snap, context) => {
+        // Get the note document
+        const question = snap.data();
+
+        // Add an 'objectID' field which Algolia requires
+        question.objectID = context.params.questionId;
+
+        // Write to the algolia index
+        const index = client.initIndex(ALGOLIA_INDEX_NAME);
+        return index.saveObject(question);
+      }
+  );
